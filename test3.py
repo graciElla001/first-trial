65ef906a923d71969e62ba0b2cdeee23eb5dcca0
graciElla001 <40826319+graciElla001@users.noreply.github.com>
2018-07-15 22:09:27 +0100

    Add files via upload
    use of a variable

---
+++ test3.py
@@ -0,0 +1,11 @@
+#greeter
+#demonstrates the use of a variable
+name= "Ijeoma"
+print(name)
+print ("Hi,", name)
+
+name = input("Hi. What's your name? ")
+print(name)
+print("Hi,", name)
+
+input('\n\nPress the enter key to exit.')
