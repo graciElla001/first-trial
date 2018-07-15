c7cebc1b5a5851eae5015dad61cbec004e75911e
graciElla001 <40826319+graciElla001@users.noreply.github.com>
2018-07-15 21:56:54 +0100

    Add files via upload
    sequence practice

---
+++ test2.py
@@ -0,0 +1,18 @@
+#sequence practice
+print ('Regardless of your \'disabilities\',you should try and make your abilities a reality.')
+print ('\t\t\tOnwuasoanya Ijeoma')
+print ('\nHere,')
+print ('it is ...')
+print ('\a')
+#word problems
+print ('If a 2000 pound pregnant hippo gives birth to a 100 pound calf,')
+print('but then eats 50 pounds of food, how much does she weigh?')
+input('Press the enter key to find out.')
+print('2000 - 100 + 50 =', '2000 - 100 + 50')
+
+print('\nIf an adventurer returns from a successful quest and buys each of')
+print('6 companions 3 bottles of ale, how many bottles are purchased?')
+input('Press the enter key to find out.')
+print('6 * 3 =", 6 * 3')
+
+input("\n\nPress the enter key to exit.")
