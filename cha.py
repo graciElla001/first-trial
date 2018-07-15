c5ed02eb8ef3d43447852d07cad3b641e674330d
graciElla <40826319+graciElla001@users.noreply.github.com>
2018-07-15 22:37:02 +0100

    Add files via upload
    joining strings

---
+++ cha.py
@@ -0,0 +1,6 @@
+#challenge
+food = input("what is your number one favourite food?")
+second = input("what is your second favourite food?")
+print("\nGrand Total:", food, "and", second)
+
+input("\n\nPress the enter key to exit.")
