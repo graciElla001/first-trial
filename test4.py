3d50fb51fc48f36cc2b695eccf3ffc80aa0bda91
graciElla001 <40826319+graciElla001@users.noreply.github.com>
2018-07-15 22:10:54 +0100

    Add files via upload
    use of alphabetical cases

---
+++ test4.py
@@ -0,0 +1,25 @@
+#quote manipulation
+# quote from IBM Chairman, Thomas Watson, in 1943
+quote = "I think there is a world market for maybe five computers."
+print("Original quote:")
+print(quote)
+
+print("\nIn uppercase:")
+print(quote.upper())
+
+print("\nIn lowercase:")
+print(quote.lower())
+
+print("\nAs a title:")
+print(quote.title())
+
+print("\nWith a minor replacement:")
+print(quote.replace("five", "millions of"))
+
+print("\nOriginal quote is still:")
+print(quote)
+
+print ("\nAs a strip")
+print (quote.strip())
+
+input("\n\nPress the enter key to exit.")
