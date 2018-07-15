c478fa62404d071e765acc5caeea569fd392eb9d
graciElla <40826319+graciElla001@users.noreply.github.com>
2018-07-15 23:06:37 +0100

    Add files via upload
    writing to a text file

---
+++ Write _It.py
@@ -0,0 +1,14 @@
+# Write It
+# Demonstrates writing to a text file
+print("Creating a text file with the write() method.")
+text_file = open("write_it.txt", "w")
+
+text_file.write("Line 1\n")
+text_file.write("This is line 2\n")
+text_file.write("That makes this line 3\n")
+
+text_file.close()
+print("\nReading the newly created file.")
+text_file = open("write_it.txt", "r")
+print(text_file.read())
+text_file.close()
