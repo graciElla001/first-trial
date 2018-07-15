1de34bd05506aa7623fba1e4807b089c8f168d63
graciElla <40826319+graciElla001@users.noreply.github.com>
2018-07-15 22:39:56 +0100

    Add files via upload
    understanding the random module

---
+++ cha4.py
@@ -0,0 +1,6 @@
+#cookie challenge
+import random
+cookie1 = random.randint(1,5)
+
+print (cookie1)
+input("\n\nPress the enter key to exit.")
---
+++ cha5.py
@@ -0,0 +1,10 @@
+#heads and tails
+import random
+head = random.randint(1,50)
+tail = random.randint(1,50)
+total = head + tail
+print ("In a hundred flips of the coin you have a total heads of", head)
+print ("And a total tails of", tail)
+print ("And in total", total)
+
+input("\n\nPress the enter key to exit.")     
