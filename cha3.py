0276c5457d0a86f4703e61fc98ca33ae22f42ebf
graciElla <40826319+graciElla001@users.noreply.github.com>
2018-07-15 22:37:57 +0100

    Add files via upload
    more on joining strings

---
+++ cha3.py
@@ -0,0 +1,7 @@
+#car salesman challenge
+price = int(input("The base price of this car is"))
+Dealer_prep = int(input("Dealer fee"))
+destination_charge = int(input("This distance will be"))
+total = price + Dealer_prep + destination_charge
+print("\nGrand Total:", total)
+input("\n\nPress the enter key to exit.")
